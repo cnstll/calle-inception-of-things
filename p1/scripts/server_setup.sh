caa7328bb433d5a9a68489c9e7bae7e9fc7f48ec
cnstll <calle@student.42.fr>
2023-10-24 19:43:27 +0200

    test for injecting config script in controll node

--- p1/scripts/server_setup.sh
+++ p1/scripts/server_setup.sh
@@ -1,3 +1,5 @@
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-backend none" K3S_TOKEN=12345 K3S_KUBECONFIG_MODE="644" sh -s -
-mkdir ~/.kube
+sleep 15
+pwd
+sudo mkdir -v ~/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube
