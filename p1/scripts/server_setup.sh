f3e2677b4366ad83a517ad2e3de034e0e721d911
cnstll <calle@student.42.fr>
2023-10-26 17:44:39 +0200

    cleaning and commenting installation scripts

--- p1/scripts/server_setup.sh
+++ p1/scripts/server_setup.sh
@@ -1,12 +1,10 @@
-whoami
-id
-pwd
-ls -a
-#rc-update add cgroups default
 SERVER_IP="192.168.56.110"
+# Env variables that will be used for the installation of k3s
 export INSTALL_K3S_EXEC="server --node-ip ${SERVER_IP} --flannel-iface eth1 --write-kubeconfig-mode 644"
+# Downloading and installing k3s
 curl -sfL https://get.k3s.io | sh -s -
 
+# Waiting for control to be fully ready in order to collect its config and its token
 mkdir -v .kube/
 KUBE_CONFIG="/etc/rancher/k3s/k3s.yaml"
 while [ ! -f ${KUBE_CONFIG} ]; do
@@ -14,10 +12,10 @@ while [ ! -f ${KUBE_CONFIG} ]; do
     echo "Waiting for kubeconfig creation..."
 done
 cp -v /etc/rancher/k3s/k3s.yaml .kube/
-echo "The k3s config has been created and copied."
 
+# Collecting controller node token that will be passed to the agent
 NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
 TOKEN=$(cat ${NODE_TOKEN})
-echo "Server Token : ${TOKEN}"
 cp ${NODE_TOKEN} /vagrant_shared/
+
 echo "Server configuration done ✔"
