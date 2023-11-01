d745eb127868a955c92063cba2fcb5216301478f
cnstll <calle@student.42.fr>
2023-11-01 11:31:42 +0100

    cleaning launch files

--- p3/scripts/launch.sh
+++ p3/scripts/launch.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+# If any issue with kubectl arise you may want to modify access rights
 # sudo chown -R $USER $HOME/.kube
 
 # Create and launch cluster with k3d
-k3d cluster create cluster-iot 
+k3d cluster create cluster-iot -p 8080:80@loadbalancer --agents 2
 echo "cluster launched ✔"
-sleep 10
+sleep 5
+
+# Creating namespaces
+kubectl create namespace dev
+kubectl create namespace argocd
+echo "dev and argocd namespaces created ✔"
+
 
 # Download and install argocd
-helm repo add argo-cd https://argoproj.github.io/argo-helm
-helm dep update confs/charts/argo-cd/
-echo "Downloaded helm chart for argocd ✔"
-helm install --create-namespace --namespace argocd argo-cd confs/charts/argo-cd/
-echo "Installed helm chart for argocd ✔"
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml > /dev/null 2>&1
+echo "Argo CD installed ✔"
 
-echo "Waiting for cluster to be ready..."
+echo "Waiting for Argo CD to be ready..."
 EXPECTED_NUM_OF_RUNNING_POD=7
 CURRENT_PODS_RUNNING=$(kubectl get pods -n argocd | grep " Running " | wc -l > /dev/null 2>&1)
 while [[ ${CURRENT_PODS_RUNNING} -ne ${EXPECTED_NUM_OF_RUNNING_POD} ]]; do
@@ -22,10 +26,9 @@ while [[ ${CURRENT_PODS_RUNNING} -ne ${EXPECTED_NUM_OF_RUNNING_POD} ]]; do
         CURRENT_PODS_RUNNING=$(kubectl get pods -n argocd | grep " Running " | wc -l)
         echo "CLUSTER INFO: ${CURRENT_PODS_RUNNING} pods out of ${EXPECTED_NUM_OF_RUNNING_POD} are running..." 
 done
-echo "Apps are ready ✔"
+echo "Argo CD is fully running ✔"
 
-# Deploying different manifest
-kubectl create namespace dev
+# Applying Project and Application for Argo 
 kubectl apply --namespace argocd -f confs/app-project.yaml
 kubectl apply --namespace argocd -f confs/wils-application.yaml
 
@@ -36,9 +39,9 @@ kubectl -n argocd patch secret argocd-secret \
     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
   }}'
 
-
+# Connection informations
 echo "Login to argo: admin"
 echo "Password to argo: password"
 echo "Enter the following commands into 2 distinct tty to access the different services:"
-echo "kubectl port-forward --namespace argocd svc/argo-cd-argocd-server 8080:443"
+echo "kubectl port-forward --namespace argocd svc/argocd-server 8080:443"
 echo "kubectl port-forward --namespace dev service/wil-playground 8888:8888"
